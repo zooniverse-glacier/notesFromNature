f1c0045f540262a0534f01ce441468da00b14b48
Chris Snyder <bumishness@gmail.com>
2014-08-19 10:47:52 -0500

    Remove unnecessary AWS configs

--- staging-build.rb
+++ staging-build.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
-
 require 'aws-sdk'
-AWS.config access_key_id: ENV['AMAZON_ACCESS_KEY_ID'], secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY']
+
 s3 = AWS::S3.new
 bucket = s3.buckets['zooniverse-demo']
 
