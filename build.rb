b5d3bfae349faea8d486a929f27be0e286a73203
Chris Snyder <bumishness@gmail.com>
2014-12-05 13:39:00 -0500

    Remove unused build file

--- build.rb
+++ build.rb
@@ -1,4 +1,5 @@
 timestamp = Time.new.strftime('%F-%T')
+`rm ./build/nfn/build.js`
 `cp ./public/nfn/build.js ./build/nfn/build-#{timestamp}.js`
 index = File.read('./build/index.html')
 index.gsub!('./nfn/build.js', "./nfn/build-#{timestamp}.js")
