067669cfa0a8ea9f8a84e6de26ec1283d48b0711
Chris Snyder <bumishness@gmail.com>
2014-12-05 10:18:03 -0500

    Stop-gap "build" script for interface code

---
+++ build.rb
@@ -0,0 +1,5 @@
+timestamp = Time.new.strftime('%F-%T')
+`cp ./public/nfn/build.js ./build/nfn/build-#{timestamp}.js`
+index = File.read('./build/index.html')
+index.gsub!('./nfn/build.js', "./nfn/build-#{timestamp}.js")
+File.open('build/index.html', 'w'){ |f| f.puts index }
